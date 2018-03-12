da2a5cc4c3f5b36bc93a1ff1342b57c159188bc7
James Priest <james-priest@outlook.com>
2018-03-12 10:44:15 -0700

    Add Ch13 Drag & Drop 15-16

---
+++ ch13-Drag-Drop/d-scramble1.js
@@ -0,0 +1,25 @@
+var squareCount = 16;
+var emptySquare;
+
+$(document).ready(function() {
+    createBoard();
+    addTiles();
+});
+
+function createBoard() {
+    for (var i = 0; i < squareCount; i++) {
+        var $square = $('<div id="square' + i + '" data-square="' +
+            i + '" class="square"></div>');
+        $square.appendTo($('#gameBoard'));
+    }
+}
+
+function addTiles() {
+    emptySquare = squareCount - 1;
+    for (var i = 0; i < emptySquare; i++) {
+        var $square = $('#square' + i);
+        var $tile = $('<div draggable="true" id="tile' + i +
+            '"  class="tile">' + (i + 1) + '</div>');
+        $tile.appendTo($square);
+    }
+}
